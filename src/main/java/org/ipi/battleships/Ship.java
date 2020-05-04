05b8b8ba6c1f66bd805d6968e82b37d04ff04a7f
Merokai <amonyak@amostack.ovh>
2020-05-04 15:36:01 +0200

    Added Ship::isOnCoordinate

--- src/main/java/org/ipi/battleships/Ship.java
+++ src/main/java/org/ipi/battleships/Ship.java
@@ -3,4 +3,8 @@ package org.ipi.battleships;
 public class Ship {
     public Ship(ShipModel model, Coordinate coordinate, Orientation orientation) {
     }
+
+    public boolean isOnCoordinate(int x, int y) {
+        return true;
+    }
 }
--- src/test/java/org/ipi/battleships/ShipTest.java
+++ src/test/java/org/ipi/battleships/ShipTest.java
@@ -1,6 +1,9 @@
 package org.ipi.battleships;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ShipTest {
 
@@ -8,5 +11,6 @@ public class ShipTest {
     @Test
     void shipTests() {
         Ship ship = new Ship(ShipModel.CARRIER, new Coordinate(2, 3), Orientation.EAST);
+        assertTrue(ship.isOnCoordinate(2, 3));
     }
 }
