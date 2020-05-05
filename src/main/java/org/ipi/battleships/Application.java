5f1678b66644ac34b13eb296f0c44626468c3f75
Merokai <amonyak@amostack.ovh>
2020-05-05 13:09:17 +0200

    Playable using console

--- src/main/java/org/ipi/battleships/Application.java
+++ src/main/java/org/ipi/battleships/Application.java
@@ -2,7 +2,19 @@ package org.ipi.battleships;
 
 public class Application {
 
-    public static void main(int... args) {
+    public static void main(String... args) {
+        ConsolePlayer player = new ConsolePlayer("Dear player");
 
+        Ship carrier = new Ship(ShipModel.CARRIER, new Coordinate(1, 2), Orientation.SOUTH);
+        Ship battleship = new Ship(ShipModel.BATTLESHIP, new Coordinate(2, 2), Orientation.SOUTH);
+        Ship cruiser = new Ship(ShipModel.CRUISER, new Coordinate(3, 2), Orientation.SOUTH);
+        Ship submarine = new Ship(ShipModel.SUBMARINE, new Coordinate(4, 2), Orientation.SOUTH);
+        Ship destroyer = new Ship(ShipModel.DESTROYER, new Coordinate(5, 2), Orientation.SOUTH);
+
+        Fleet fleet = new Fleet(carrier, battleship, cruiser, submarine, destroyer);
+
+        Game game = new Game(fleet, player);
+        game.play();
     }
+
 }
