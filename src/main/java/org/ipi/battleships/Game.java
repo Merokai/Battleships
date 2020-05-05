57f59ecf7fd84d51f778a42e647399c5a021ec16
Merokai <amonyak@amostack.ovh>
2020-05-05 09:33:46 +0200

    Added Game entity

---
+++ src/main/java/org/ipi/battleships/Game.java
@@ -0,0 +1,10 @@
+package org.ipi.battleships;
+
+public class Game {
+
+    private final Fleet fleet;
+
+    public Game(Fleet fleet) {
+        this.fleet = fleet;
+    }
+}
---
+++ src/test/java/org/ipi/battleships/GameTest.java
@@ -0,0 +1,21 @@
+package org.ipi.battleships;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameTest {
+
+    @Test
+    void gameTests() {
+        Ship carrier = new Ship(ShipModel.CARRIER, new Coordinate(1, 2), Orientation.SOUTH);
+        Ship battleship = new Ship(ShipModel.BATTLESHIP, new Coordinate(2, 2), Orientation.SOUTH);
+        Ship cruiser = new Ship(ShipModel.CRUISER, new Coordinate(3, 2), Orientation.SOUTH);
+        Ship submarine = new Ship(ShipModel.SUBMARINE, new Coordinate(4, 2), Orientation.SOUTH);
+        Ship destroyer = new Ship(ShipModel.DESTROYER, new Coordinate(5, 2), Orientation.SOUTH);
+
+        Fleet fleet = new Fleet(carrier, battleship, cruiser, submarine, destroyer);
+
+        Game game = new Game(fleet);
+    }
+}
